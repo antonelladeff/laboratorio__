6311187f81c0a66d6c963f42b8a8dc027a4be61e
German rojas <german_rojas76@hotmail.com>
2025-10-30 20:57:12 -0300

    chore: establecer puerto de desarrollo a 3001; feat: agregar ToasterClient y mover Toaster a componente cliente; feat: traer frontend desde repo antiguo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import "./globals.css";
 import NavBar from "../components/Navbar";
-import { Toaster } from 'react-hot-toast';
+import ToasterClient from "../components/ToasterClient";
 
 export default function RootLayout({
   children,
@@ -15,38 +15,8 @@ export default function RootLayout({
         <NavBar />
         {/* Renderizar el contenido de cada página */}
         {children}
-        {/* Toaster global para controlar duración de los toasts */}
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 8000,
-            style: {
-              minWidth: '400px',
-              minHeight: '60px',
-              fontSize: '1.2rem',
-            },
-            success: {
-              duration: 5000,
-              style: {
-                background: '#e0f7fa',
-                color: '#00796b',
-                minWidth: '400px',
-                minHeight: '60px',
-                fontSize: '1.2rem',
-              },
-            },
-            error: {
-              duration: 10000,
-              style: {
-                background: '#ffebee',
-                color: '#c62828',
-                minWidth: '400px',
-                minHeight: '60px',
-                fontSize: '1.2rem',
-              },
-            },
-          }}
-        />
+        {/* Toaster global (cliente) */}
+        <ToasterClient />
       </body>
     </html>
   );
